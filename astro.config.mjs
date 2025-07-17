415f8d620303c0efd2f7648481fd160d2ccc09df
Manuel Tonello <49204166+txnello@users.noreply.github.com>
2025-07-17 20:25:55 +0200

    Installata estensione Astro Vercel per la gestione delle immagini

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -9,4 +10,5 @@ import icon from 'astro-icon';
 export default defineConfig({
     site: 'https://example.com',
     integrations: [mdx(), sitemap(), icon()],
+    adapter: vercel({imageService: true}),
 });
